9e6adedced41cb0cd5dc772d94e22541c4c89f30
majesty2017 <reborndzre@gmail.com>
2023-03-15 10:41:39 +0000

    Remove broken images

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -20,11 +20,13 @@ const Row = ({title, fetchUrl, isLargeRow = false}) => {
         <h2>{title}</h2>
         <div className="row__posters">
           {movies.map(movie => (
-            isLargeRow && movie.poster_path |
-            <img src={`${base_url}${
-                isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`} alt={movie.name} key={movie.id} className={`row__poster ${isLargeRow && 'row__posterLarge'}`} />
-        ))}
+            (isLargeRow && movie.poster_path) ||
+            (!isLargeRow && movie.backdrop_path)) && (
+              <img src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`} alt={movie.name} key={movie.id} className={`row__poster ${isLargeRow && 'row__posterLarge'}`} />
+            )
+        )}
         </div>
     </div>
   )
